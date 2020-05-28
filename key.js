5aab403505a3518e9fc6190ae73d94d798f68ddc
Pritam Das <49154874+pritam199902@users.noreply.github.com>
2020-05-28 12:59:19 +0530

    Add files via upload

--- key.js
+++ key.js
@@ -1,4 +1,5 @@
 module.exports = {
-    dbUrl: "mongodb://localhost:27017/react_node",
+    // dbUrl: "mongodb://localhost:27017/react_node",
+    dbUrl: "mongodb+srv://user:pritam@123@cluster0-tl2so.mongodb.net/test?retryWrites=true&w=majority",
     tokenKey: "asdfghjkl", 
 };
